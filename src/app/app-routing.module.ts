785b1e6d8dfabf4ca34285458bc99e875fe8fc75
Berezin Stepan <stepan-berezin@list.ru>
2022-07-01 00:26:24 +1000

    Внес изменений по мелочи

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,9 @@ const routes: Routes = [
   { path: "src/app/colors", component: ColorsComponent },
   { path: "src/app/user", component: UserComponent },
   { path: "src/app/comments", component: CommentsComponent },
-  { path: "src/app/todos", component: TodosComponent }
+  { path: "src/app/todos", component: TodosComponent },
+  { path: "src/app/albums", redirectTo: "src/app/user" },
+  { path: "**", redirectTo: "/" },
 ];
 
 @NgModule({
